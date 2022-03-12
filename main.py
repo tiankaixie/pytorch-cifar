82aae22dd1511333a609af16aba874c825c38e12
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2022-03-11 18:36:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ trainset = torchvision.datasets.CIFAR10(
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-print("train loader shape: ", trainloader.dataset.__len__())
+print("train loader size: ", trainloader.__len__())
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
