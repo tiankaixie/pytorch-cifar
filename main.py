d5d0e1bb9c6040c166dc00daea11d6ceb1d1a676
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2022-03-11 18:52:30 -0700

    split

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 print("train loader size: ", trainloader.__len__())
+trainloader_part1 = trainloader[:150]
+trainloader_part2 = trainloader[150:]
+
+trainloader = trainloader_part1
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
